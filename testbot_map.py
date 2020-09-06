c201c3d5cc7d6f9ba219efb48788b277196a5e2f
sery270 <sery270@naver.com>
2020-09-07 02:29:08 +0900

    [Implement Features] !show_map ft develop start

--- testbot_map.py
+++ testbot_map.py
@@ -7,7 +7,20 @@ client = commands.Bot(command_prefix = '-')
 @client.event 
 async def on_ready():
     await client.change_presence(status=discord.Status.online)
-    await client.change_presence(activity=discord.Game(name="게임 하는중"))
+    await client.change_presence(activity=discord.Game(name="당신의 위치를 파악"))
     print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+@client.event
+async def on_message(message):
+    # if this message is sent by bot itself, do nothing.
+    if message.author == client.user:
+        return
+    
+    #show map feature
+    if message.content.startswith('!show_map'):
+        await message.channel.send('Loading...Map..')
+    
+
+
 
 client.run(os.environ['token_map'])
