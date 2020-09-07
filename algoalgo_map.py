b089ba7d526fb61950732672b06e588c777f5a6b
sery270 <sery270@naver.com>
2020-09-07 20:24:35 +0900

    [Fix Bugs] %d format error fixed

--- algoalgo_map.py
+++ algoalgo_map.py
@@ -57,11 +57,11 @@ def setmap(cmd):
     args = cmd.split()
     if len(args) != 4:
         return "Usage : !set_map <id> <feature> <ahead_to>"
-    # !set_map <이름> <학번> <이동할 위치>
+    # !set_map <칸 순서> <normal: 0, ladder: 1, snake: 2, boss: 3> <이동할 위치>
 
-    id = args[1]
-    feature = args[2]
-    ahead_to = args[3]
+    id = int(args[1])
+    feature = int(args[2])
+    ahead_to = int(args[3])
    
 
     sql = "insert into member (id, feature, ahead_to) value (%d, %d, %d);"
