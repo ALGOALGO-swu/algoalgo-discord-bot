763d965299485feda94384bdd0fa0190ff54bc4b
sery270 <sery270@naver.com>
2020-09-22 19:54:32 +0900

    [Implement Features] role id chk

--- testbot_map.py
+++ testbot_map.py
@@ -4,6 +4,7 @@ import algoalgo_map
 import os
 
 client = commands.Bot(command_prefix = '-')
+admin = 742625793276116992
 
 @client.event 
 async def on_ready():
@@ -25,55 +26,33 @@ async def on_message(message):
         await message.channel.send(result)
         await message.channel.send(embed=embed)
     
-    #set map feature
+    #admin :: set map feature
     if message.content.startswith('!set_map'):
-        result = algoalgo_map.setmap(message.content)
+        result = f"[*] the adming permission required."
+        if message.author.top_role.id == admin:
+            result = algoalgo_map.setmap(message.content)
         await message.channel.send(result)
-    
 
-    # if message.guild.roles[-1] == message.author.roles[-1]:
-        
- 
-
-
-    # if message.content.startswith('!set_map'):
-    #     # admin = message.author.roles.find
-    #     # admin = discord.utils.get(ctx.guild.roles, name = "db_admin")
-        
-    #     result = f"[*] the permission required."
-    #     # if message.author.roles.has('db_admin')
-    #     # if message.guild.get in get(ctx.guild.roles, name = "db_admin").members:
-    #     # if get(ctx.guild.roles, name = "db_admin") in discord.member.roles:
-            
-    #     if (message.author.guild_permissions.administrator):
-    #         try:
-    #             result = algoalgo_map.setmap(message.content)
-    #         except Exception as e:
-    #             await message.channel.send(embed=discord.Embed(title="에러 발생", description = str(e), color = 0xff0000))
-    #     else:
-    #         await message.channel.send(embed=discord.Embed(title="권한 부족", description = message.author.mention + "님은 채널을 관리 할 수 있는 권한이 없습니다.", color = 0xff0000))
-    #     await message.channel.send(result)
-
-
-    #test :: getLocType
+    #admin :: getLocType
     if message.content.startswith('!getLocType'):
-        result, LocFeatureInfo, nowLoc = algoalgo_map.getLocType(message.content)
-        embed = discord.Embed(title = f"""== the feature of the **{nowLoc}** location ==""", description=LocFeatureInfo, color = 0x6b9560)
+        result = f"[*] the adming permission required."
+        if message.author.top_role.id == admin:
+            result, LocFeatureInfo, nowLoc = algoalgo_map.getLocType(message.content)
+            embed = discord.Embed(title = f"""== the feature of the **{nowLoc}** location ==""", description=LocFeatureInfo, color = 0x6b9560)
         await message.channel.send(result)
         await message.channel.send(embed=embed)
 
-    #test :: getPlayers
+    #admin :: getPlayers
     if message.content.startswith('!getPlayers'):
-        result , Locinfo, nowLoc = algoalgo_map.getPlayers(message.content)
-        embed = discord.Embed(title = f"""== the player list in the **{nowLoc}** location ==""", description=Locinfo, color = 0x6b9560)
+        result = f"[*] the adming permission required."
+        if message.author.top_role.id == admin:
+            result , Locinfo, nowLoc = algoalgo_map.getPlayers(message.content)
+            embed = discord.Embed(title = f"""== the player list in the **{nowLoc}** location ==""", description=Locinfo, color = 0x6b9560)
         await message.channel.send(result)
         await message.channel.send(embed=embed)
         
-    # printing top_role id 
+    #admin ::  printing top_role id 
     if message.content.startswith('!role_id'):
-        # RList = ""
-        # for i in range(len(message.guild.roles)):
-        #     RList += message.author.roles[i].mention
-        await message.channel.send(embed=discord.Embed(title="권한 info", description = f"""== {message.author}님의 권한은 {message.author.top_role.name}이고\n 해당 권한의 id는 {message.author.top_role.id} 입니다.==""", color = 0xff0000))
+        await message.channel.send(embed=discord.Embed(title=f"""== {message.author}'s the highest role INFO ==""", description = f""" the highest role :: {message.author.top_role.name}\n the highest role id :: {message.author.top_role.id} """, color = 0x6b9560))
         
 client.run(os.environ['token_map'])
