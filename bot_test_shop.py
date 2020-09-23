9fc673aa8efd477bdef4c3d2758c26c04886ed94
Yujin Lee <proqk@naver.com>
2020-09-23 18:23:23 +0900

    error fix

--- bot_test_shop.py
+++ bot_test_shop.py
@@ -19,7 +19,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # show shop items detail info
+    # show items detail info
     if message.content.startswith('!showshopinfo'):
         embed = discord.Embed(title="ALGOALGO SHOP BOT",description="SHOP 아이템 목록", color=0x00aaaa, inline=True)
         embed.add_field(name="STEP🦶", value="저희 게임 일반 모드에서의 전진 아이템입니다. 보드 판 위의 문제(백준 알고리즘 문제)를 해결 했을 때, 앞으로 1칸 전진할 수 있는 기회를 제공해주는 아이템입니다. 하루 최대 2개 까지 구매가능 합니다. 다만, 두 번째 구매시엔 가격이 두 배로(10pt로) 상승합니다. 즉, 하루에 STEP을 사용하여 2칸까지 전진할 수 있습니다.", inline=False)
@@ -32,30 +32,32 @@ async def on_message(message):
         embed.add_field(name="BOMB💣", value="저희 게임 보스레이드 모드에서의 보스 공격 아이템입니다. 구매 즉시 보스에게 100 데미지를 주게됩니다. 다만 각 개인들은 해당 아이템을 5개 까지만 구매 가능합니다.", inline=False)
         await message.channel.send(embed=embed)
         await message.channel.send("Please enter the item you want\nSTEP🦶, REDEMPTION🛡, SNAKE🐍, ASSASSIN🗡, STUN⚔️\nUsage: Usage: !buyitem <item name> <number>")
-        
+
     # show shop items info
     if message.content.startswith('!shop'):
-        #mbed.set_image(url="https://blog.kakaocdn.net/dn/b4numP/btqIrvqfcvg/Hm88ead0XHCjQnyKjoSO91/img.png")
-        embed = discord.Embed(title="Please enter the item you want")
+        embed = discord.Embed(title="유후! 여름 빅 세일입니다\nSTEP과 STUN, 제가 직접 만든 SNAKE도 반값 할인 중입니다")
+        embed.set_image(url="https://blog.kakaocdn.net/dn/b4numP/btqIrvqfcvg/Hm88ead0XHCjQnyKjoSO91/img.png")
         embed.add_field(name="STEP🦶", value="3pt", inline=True)
         embed.add_field(name="REDEMPTION🛡", value="5pt", inline=True)
         embed.add_field(name="SNAKE🐍", value="10pt", inline=True)
         embed.add_field(name="ASSASSIN🗡", value="6pt", inline=True)
         embed.add_field(name="STUN⚔️", value="6pt", inline=True)
         await message.channel.send(embed=embed)
-        await message.channel.send("Usage: !buyitem <item name> <number>")     
-        
-    # buy items
+        await message.channel.send("**지금 당장 구매하세요!** -->> !buyitem <item name> <number>")
+
+    # buy items 
     if message.content.startswith('!buyitem'):
-        result, pointinfo = items.point_check(message.author)
+        result, pointinfo = algoalgo_shop.point_check(message.author)
         await message.channel.send("현재 보유 포인트: "+pointinfo)
         if int(pointinfo)>0:
-            result = items.buy_item(str(message.author), message.content)
+            result = algoalgo_shop.buy_item(str(message.author), message.content)
             await message.channel.send(result)
         else:
             await message.channel.send("포인트가 부족합니다. 구매를 종료합니다")
-    if message.content.startswith('!ptset'):
-        result = items.setpoint(message.author)
-        await message.channel.send(result)
 
-client.run('')
+    # for dev-get 10 point
+    # if message.content.startswith('!ptset'):
+    #     result = algoalgo_shop.setpoint(message.author)
+    #     await message.channel.send(result)
+
+client.run('-')
