bc1714b97cd6e01bd45c2ab536aa0689d9c9c887
sery270 <sery270@naver.com>
2020-09-30 16:46:09 +0900

    [Refactor Codes] admin key test

--- testbot_map.py
+++ testbot_map.py
@@ -4,8 +4,7 @@ import algoalgo_map
 import os
 
 client = commands.Bot(command_prefix = '-')
-#admin = 742625793276116992
-admin = 751022850466381845
+admin = 742625793276116992
 
 @client.event 
 async def on_ready():
