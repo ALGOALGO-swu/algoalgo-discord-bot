540dfb6b2f2d7ba6ed58ec77970d8c385ccd732b
Yujin Lee <proqk@naver.com>
2020-10-01 14:36:46 +0900

    Update algoalgo_main.py

--- algoalgo_main.py
+++ algoalgo_main.py
@@ -2,15 +2,11 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import os
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime
 import algoalgo_member
 import algoalgo_shop
 
 
 client = discord.Client()
-admin = 742625793276116992
-
 
 @client.event
 async def on_ready():
@@ -21,11 +17,6 @@ async def on_ready():
     print("디스코드봇 버전:" + str(discord.__version__))
     print('------')
     await client.change_presence(status=discord.Status.online, activity=discord.Game("테스트"))
-
-@client.event
-async def db_refresh():
-    print(datetime.now())
-    algoalgo_member.refresh()
 
 @client.event
 async def on_message(message):
@@ -49,45 +40,6 @@ async def on_message(message):
         embed = discord.Embed(title = f"USERINFO_{message.author}", description=userinfo, color = 0xffffff)
         await message.channel.send(result)
         await message.channel.send(embed=embed)
-
-    if message.content.startswith('!refresh'):
-        result = f"[!] Admin Permission Required."
-        if message.author.top_role.id == admin:
-            result = algoalgo_member.refresh()
-        await message.channel.send(result)
-
-    if message.content.startswith('!addpoint'):
-        result = f"[!] Admin Permission Required."
-        if message.author.top_role.id == admin:
-            result = algoalgo_member.addpoint(message.content)
-        await message.channel.send(result)
-
-    if message.content.startswith('!list_achievement'):
-        result = algoalgo_member.list_achievement()
-        if result.split()[0] != '[!]':
-            embed = discord.Embed(title="Achievement List", description=result, color=0xffffff)
-            await message.channel.send(f"[*] Successfully Inquired Achievement List")
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send(result)
-
-    if message.content.startswith('!random_bj'):
-        result = algoalgo_member.random_bj(str(message.author), message.content)
-        if result.split()[0] != '[!]':
-            embed = discord.Embed(title="Try This!", description=result, color=0xffffff)
-            await message.channel.send(f"[*] Successfully Found A Random Baekjoon Problem")
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send(result)
-
-    if message.content.startswith('!daily_baekjoon'):
-        result = algoalgo_member.daily_baekjoon(str(message.author), message.content)
-        await message.channel.send(result)
-
-    if message.content.startswith('!unlock'):
-        result = algoalgo_member.unlock(str(message.author))
-        await message.channel.send(result)
-
 
     # show items detail info
     if message.content.startswith('!showshopinfo'):
@@ -125,47 +77,4 @@ async def on_message(message):
         else:
             await message.channel.send("포인트가 부족합니다. 구매를 종료합니다")
 
-    # show boss shop items detail info
-    if message.content.startswith('!boss_shop'):
-        embed = discord.Embed(title="ALGOALGO BOSS SHOP BOT",description="BOSS SHOP 아이템 목록", color=0x00aaaa, inline=True)
-        embed.add_field(name="CAFFEINE🍺", value="3포인트. 본인의 다음 공격 데미지가 2배가 됩니다", inline=False)
-        embed.add_field(name="REDBULL💊", value="5포인트. 본인의 다음 공격 데미지가 3배가 됩니다", inline=False)
-        embed.add_field(name="BOMB💣", value="6포인트. 구매 즉시 자동 사용으로, 보스에게 100 데미지 공격을 합니다", inline=False)
-        await message.channel.send(embed=embed)
-        #await message.channel.send("Please enter the item you want\CAFFEINE🍺, REDBULL💊, BOMB💣\nUsage: !buybossitem <item name> <number>")
-        msg = await message.channel.send("원하는 아이템 이모지를 클릭하세요")
-        await msg.add_reaction("🍺") #caffeine
-        await msg.add_reaction("💊") #red bull
-        await msg.add_reaction("💣") #bomb
-        
-        def check(reaction, user):
-            return user == message.author and (str(reaction.emoji) == '🍺' or str(reaction.emoji) == '💊' or str(reaction.emoji) == '💣')
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=30.0, check=check)
-        except asyncio.TimeoutError:
-            await message.channel.send('👎')
-        else:
-            #await message.channel.send('👍')
-            whatemoji=""
-            if str(reaction.emoji) == '🍺':
-                whatemoji="CAFFEINE"
-            elif str(reaction.emoji) == '💊':
-                whatemoji="REDBULL"
-            elif str(reaction.emoji) == '💣':
-                whatemoji="BOMB"
-            
-            result, pointinfo = algoalgo_shop.point_check(message.author)
-            await message.channel.send("현재 보유 포인트: "+pointinfo)
-            if int(pointinfo)>0:
-                gogo = "!buyitem "+whatemoji+" 1"
-                result = algoalgo_shop.buy_item(str(message.author), gogo)
-                await message.channel.send(result)
-            else:
-                await message.channel.send("포인트가 부족합니다. 구매를 종료합니다")
-
-sched = AsyncIOScheduler()
-sched.add_job(db_refresh, 'cron', hour=0)
-sched.start()
-
 client.run(os.environ['discord-token'])
-
